0cd242da9ba640a7e2cc5d6a5c2c33a1c6359932
VoinaRares <voinarares14@gmail.com>
2025-09-13 16:21:41 +0300

    Add environment configuration for BACKENDAPI in Next.js setup

--- interactive-map/next.config.ts
+++ interactive-map/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    BACKENDAPI: process.env.BACKENDAPI,
+  }
 };
 
 export default nextConfig;
